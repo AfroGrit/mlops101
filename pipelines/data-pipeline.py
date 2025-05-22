b007810c2cdfb6ba977b0de915d26978ddafc1f7
AfroGrit <jlkitunka@gmail.com>
2025-05-22 17:50:07 +0000

    update docker registry

--- pipelines/data-pipeline.py
+++ pipelines/data-pipeline.py
@@ -1,10 +1,9 @@
 import kfp
 from kfp import dsl
 
-@dsl.pipeline(name='mlops101-pipeline')
+
+@dsl.pipeline(name="mlops101-pipeline")
 def data_pipeline():
     spark_task = dsl.ContainerOp(
-        name='spark-job1',
-        image='your-registry/mlops101-spark-job1:latest',
-        arguments=[]
+        name="spark-job1", image="afrogrit/mlops101-spark-job1:latest", arguments=[]
     )
